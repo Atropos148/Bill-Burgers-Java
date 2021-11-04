03c27b20fa9e4f73031e7fbf9fe3ea8e503a64b7
atropos148 <tibor.h148@gmail.com>
2021-11-04 14:51:15 +0100

    Added Bread, Meat class + support for additions in Burger

--- BurgerShopManager/src/Burger.java
+++ BurgerShopManager/src/Burger.java
@@ -5,11 +5,39 @@ public class Burger {
 	private int maxAdditions;
 	private double price;
 
+	private Addition additionFirst;
+	private Addition additionSecond;
+	private Addition additionThird;
+	private Addition additionFourth;
+
 	public Burger(int breadType, int meatType, double price) {
 		this.breadType = breadType;
 		this.meatType = meatType;
 		this.price = price;
 		this.name = "The Standard";
 		this.maxAdditions = 4;
+
+		this.additionFirst = null;
+		this.additionSecond = null;
+		this.additionThird = null;
+		this.additionFourth = null;
 	}
+
+	public Addition getAdditionFirst() {
+		return additionFirst;
+	}
+
+	public Addition getAdditionSecond() {
+		return additionSecond;
+	}
+
+	public Addition getAdditionThird() {
+		return additionThird;
+	}
+
+	public Addition getAdditionFourth() {
+		return additionFourth;
+	}
+
+
 }
