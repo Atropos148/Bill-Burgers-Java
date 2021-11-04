efadf638a25275dc46b0c617bcea7c7afdb934e5
atropos148 <tibor.h148@gmail.com>
2021-11-04 14:59:38 +0100

    Added addAddition function in Burger

--- BurgerShopManager/src/Burger.java
+++ BurgerShopManager/src/Burger.java
@@ -1,8 +1,9 @@
 public class Burger {
 	private String name;
-	private int breadType;
-	private int meatType;
+	private Bread breadType;
+	private Meat meatType;
 	private int maxAdditions;
+	private int currentAdditions;
 	private double price;
 
 	private Addition additionFirst;
@@ -10,12 +11,13 @@ public class Burger {
 	private Addition additionThird;
 	private Addition additionFourth;
 
-	public Burger(int breadType, int meatType, double price) {
+	public Burger(Bread breadType, Meat meatType) {
 		this.breadType = breadType;
 		this.meatType = meatType;
-		this.price = price;
+		this.price = 2.0;
 		this.name = "The Standard";
 		this.maxAdditions = 4;
+		this.currentAdditions = 0;
 
 		this.additionFirst = null;
 		this.additionSecond = null;
@@ -37,6 +39,23 @@ public class Burger {
 
 	public Addition getAdditionFourth() {
 		return additionFourth;
+	}
+
+	public void addAddition(Addition newAddition) {
+		if (currentAdditions + 1 <= maxAdditions) {
+			if (additionFirst == null) {
+				additionFirst = newAddition;
+			} else if (additionSecond == null) {
+				additionSecond = newAddition;
+			} else if (additionThird == null) {
+				additionThird = newAddition;
+			} else {
+				additionFourth = newAddition;
+			}
+			System.out.println(newAddition.getName() + " added to burger " + this.name);
+		} else {
+			System.out.println("You cannot add more additions. Max additions:" + maxAdditions);
+		}
 	}
 
 
